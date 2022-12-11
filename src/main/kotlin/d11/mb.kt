bfecb191a36104a5abe3a3ceefe6759195a5d550
Rahul Jain <rahulj51@gmail.com>
2022-12-11 09:07:03 +0100

    monks working

--- src/main/kotlin/d11/mb.kt
+++ src/main/kotlin/d11/mb.kt
@@ -3,15 +3,44 @@ package d11
 import common.readInputFileAsString
 import java.lang.IllegalArgumentException
 
-data class Monkey(
+data class Monk(
     val items: MutableList<Int>,
     val testFactor: Int,
     val throwsTo: Pair<Int, Int>,
-    val operation: ArrayDeque<String>
+    val operation: ArrayDeque<String>,
+    val comfortFactor: Int = 3
 ) {
 
+    fun turn(monks: List<Monk>) {
+        this.items.forEach { throwTo(it, monks) }
+        this.items.clear()
+    }
+
+    fun throwTo(item:Int, monks: List<Monk>) {
+        val newWL = operate(item) / comfortFactor
+        val test = newWL % testFactor == 0
+
+        val throwTo = if (test) throwsTo.first else throwsTo.second
+        monks[throwTo].items.add(newWL)
+    }
+
+    fun operate(oldWL : Int): Int {
+
+        val operator = operation.first()
+        val operand  =  when (operation.last()) {
+            "old" -> oldWL
+            else -> operation.last().toInt()
+        }
+
+        return  when (operator) {
+            "*" -> oldWL * operand
+            "+" -> oldWL + operand
+            else -> oldWL //assume this for now to reduce complexity
+        }
+    }
+
     companion object Parser {
-        fun parse(str: String): Monkey {
+        fun parse(str: String): Monk {
             val lines = str.split("\n".toRegex()).map { it.trim() }.drop(1)
             println(lines)
             val items = items(lines[0]).toMutableList()
@@ -19,7 +48,7 @@ data class Monkey(
             val throwsTo = throwsTo(lines.subList(3,5))
             val operation = operation(lines[1])
 
-            return Monkey(items, testFactor, throwsTo, operation)
+            return Monk(items, testFactor, throwsTo, operation)
 
         }
 
@@ -27,7 +56,7 @@ data class Monkey(
 
             val op = s.substringAfter("=").trim()
             val stack = ArrayDeque<String>()
-            stack.addAll(op.split(" ").map { it.trim() })
+            stack.addAll(op.split(" ").map { it.trim() }.drop(1))
             return stack
         }
 
@@ -50,17 +79,22 @@ data class Monkey(
         private fun items(s: String): List<Int> {
             return s.substringAfter(":").trim().split(",").map { it.trim().toInt() }
         }
-
-
     }
 
 
 }
 
-
+//just for fun, these are curious monks, not monkeys
 fun main() {
-    val monkeynotes = readInputFileAsString("src/main/kotlin/d11/input.dat")
-    val monks = monkeynotes.split("\n\n".toRegex()).map { (Monkey).parse(it) }
+    val monkNotes = readInputFileAsString("src/main/kotlin/d11/input.dat.smol")
+    val monks = monkNotes.split("\n\n".toRegex()).map { (Monk).parse(it) }
+    println(monks)
+
+    for (i in (1..20)) {
+        monks.forEach { m -> m.turn(monks)}
+    }
+
+
     println(monks)
 
 }
