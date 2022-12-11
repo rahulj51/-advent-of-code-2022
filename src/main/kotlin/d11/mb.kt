9a4c7ffb74cf91b9722adc2350777c22f2645f6d
Rahul Jain <rahulj51@gmail.com>
2022-12-11 11:01:07 +0100

    fixed overflow bug

--- src/main/kotlin/d11/mb.kt
+++ src/main/kotlin/d11/mb.kt
@@ -2,47 +2,54 @@ package d11
 
 import common.readInputFileAsString
 import java.lang.IllegalArgumentException
+import java.math.BigInteger
 
 data class Monk(
-    val items: MutableList<Int>,
-    val testFactor: Int,
+    val items: MutableList<Long>,
+    val testFactor: Long,
     val throwsTo: Pair<Int, Int>,
     val operation: ArrayDeque<String>,
-    val comfortFactor: Int = 3
+    val comfortFactor: Long = 3L,
+    var inspections: Int = 0
 ) {
 
     fun turn(monks: List<Monk>) {
         this.items.forEach { throwTo(it, monks) }
+        this.inspections += this.items.size
         this.items.clear()
     }
 
-    fun throwTo(item:Int, monks: List<Monk>) {
+    fun throwTo(item:Long, monks: List<Monk>) {
         val newWL = operate(item) / comfortFactor
-        val test = newWL % testFactor == 0
+        val test = newWL % testFactor == 0L
 
         val throwTo = if (test) throwsTo.first else throwsTo.second
         monks[throwTo].items.add(newWL)
     }
 
-    fun operate(oldWL : Int): Int {
+    fun operate(oldWL : Long): Long {
 
         val operator = operation.first()
         val operand  =  when (operation.last()) {
             "old" -> oldWL
-            else -> operation.last().toInt()
+            else -> operation.last().toLong()
         }
 
-        return  when (operator) {
+        val x = when (operator) {
             "*" -> oldWL * operand
             "+" -> oldWL + operand
             else -> oldWL //assume this for now to reduce complexity
         }
+
+        if (x < 0) {
+            println("pause here")
+        }
+        return x
     }
 
     companion object Parser {
         fun parse(str: String): Monk {
             val lines = str.split("\n".toRegex()).map { it.trim() }.drop(1)
-            println(lines)
             val items = items(lines[0]).toMutableList()
             val testFactor = testFactor(lines[2])
             val throwsTo = throwsTo(lines.subList(3,5))
@@ -72,12 +79,12 @@ data class Monk(
             return pair
         }
 
-        private fun testFactor(s: String): Int {
-            return s.substringAfter("by").trim().toInt()
+        private fun testFactor(s: String): Long {
+            return s.substringAfter("by").trim().toLong()
         }
 
-        private fun items(s: String): List<Int> {
-            return s.substringAfter(":").trim().split(",").map { it.trim().toInt() }
+        private fun items(s: String): List<Long> {
+            return s.substringAfter(":").trim().split(",").map { it.trim().toLong() }
         }
     }
 
@@ -86,15 +93,24 @@ data class Monk(
 
 //just for fun, these are curious monks, not monkeys
 fun main() {
-    val monkNotes = readInputFileAsString("src/main/kotlin/d11/input.dat.smol")
+    val monkNotes = readInputFileAsString("src/main/kotlin/d11/input.dat")
     val monks = monkNotes.split("\n\n".toRegex()).map { (Monk).parse(it) }
     println(monks)
 
     for (i in (1..20)) {
+        println("in round $i")
+        if (i == 16) {
+            println("pause here")
+        }
         monks.forEach { m -> m.turn(monks)}
+        println(monks)
+        println()
     }
 
 
     println(monks)
+    val inspections = monks.map { it.inspections }
+    println(inspections)
+//    println(inspections[0] * inspections[1])
 
 }
