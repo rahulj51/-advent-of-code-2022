ba7866770a3cd0bb1c3723c8a6ece41f15625f8c
Rahul Jain <rahulj51@gmail.com>
2022-12-13 17:50:30 +0100

    d13 p2

--- src/main/kotlin/d13/packets.kt
+++ src/main/kotlin/d13/packets.kt
@@ -4,16 +4,47 @@ import common.readInputFileAsString
 
 fun main() {
     val input = readInputFileAsString("src/main/kotlin/d13/input.dat")
+    solve1(input)
+    solve2(input)
 
+
+}
+
+fun solve2(input:String) {
+
+    val divider1 = "[[2]]"
+    val divider2 = "[[6]]"
+    val packetsStr = input.split("\n\n".toRegex()).flatMap { pairStr -> pairStr.split("\n") } + divider1 + divider2
+    val packets = packetsStr.map { it.parse()}
+
+    val sorted = packets.sortedWith(Comparator { o1, o2 ->
+        when (compare(o1,o2)) {
+            null -> 0
+            true -> -1
+            false -> 1
+        }
+    })
+
+    println((sorted.indexOf(divider1.parse())+1) * (sorted.indexOf(divider2.parse())+1))
+
+
+}
+
+
+fun solve1(input: String) {
     val pairsStr = input.split("\n\n".toRegex()).map { pairStr -> pairStr.split("\n") }
 
     val pairs = pairsStr.map { Pair(it.first().parse(), it.last().parse()) }
 
 //    println(compare(pairs[1]))
-    println(pairs.map { compare(it) })
-    println(pairs.map { compare(it) }.mapIndexed { index, b -> if(b!!) index+1 else 0 })
+//    println(pairs.map { compare(it) })
+//    println(pairs.map { compare(it) }.mapIndexed { index, b -> if(b!!) index+1 else 0 })
     println(pairs.map { compare(it) }.mapIndexed { index, b -> if(b!!) index+1 else 0 }.sum())
+}
 
+
+fun compare(left: List<Any>, right:List<Any>): Boolean? {
+    return compareVal(left, right)
 }
 
 fun compare(pair: Pair<List<Any>, List<Any>>): Boolean? {
