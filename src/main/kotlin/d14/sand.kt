3883f860ac84dd037bfa58be38b1a9345f96c2f4
Rahul Jain <rahulj51@gmail.com>
2022-12-14 17:14:29 +0100

    Fixed. But ugly and not idiomatic. Perhaps can break it down into two classes with overriding

--- src/main/kotlin/d14/sand.kt
+++ src/main/kotlin/d14/sand.kt
@@ -31,7 +31,7 @@ data class Point (
     }
 }
 
-data class Cave2(
+data class Cave(
     var occupied: MutableSet<Point>
 ) {
     val sandSource = Point(500,0)
@@ -39,7 +39,7 @@ data class Cave2(
     val maxX = occupied.maxBy { it.x }.x
     val maxY = occupied.maxBy { it.y }.y
 
-    fun outOfBounds(p: Point): Boolean {
+    fun outOfBoundsP1(p: Point): Boolean {
         return (p.x < minX || p.x > maxX || p.y > maxY)
     }
 
@@ -52,7 +52,7 @@ data class Cave2(
         return occupied.containsAll(xxx)
     }
 
-    fun dropSand(from: Point = sandSource): Point? {
+    fun dropSandP2(from: Point = sandSource): Point? {
 
         if (uptoTop()) return null
 
@@ -63,31 +63,29 @@ data class Cave2(
             return from
         } else {
             val next = possibles.filterNot { occupied.contains(it) }.first()
+            return dropSandP2(next)
+        }
+    }
+
+    fun dropSand(from: Point = sandSource): Point? {
+
+        if (outOfBoundsP1(from)) return null
+
+        val possibles = listOf<Point>(from.down(), from.leftDown(), from.rightDown())
+        if (occupied.containsAll(possibles)) {
+            //can't go any further
+            occupied.add(from)
+            return from
+        } else {
+            val next = possibles.filterNot { occupied.contains(it) }.first()
             return dropSand(next)
         }
     }
 
-    //non recursive
-    fun dropSand2(from: Point = sandSource): Point? {
-
-        var curr = from
-        var possibles = listOf<Point>(curr.down(), curr.leftDown(), curr.rightDown())
-        while (! occupied.containsAll(possibles)) {
-            //keep flowing down
-            if (outOfBounds(curr)) {
-                return null
-            } else {
-                curr = possibles.filterNot { occupied.contains(it) }.first()
-                possibles = listOf<Point>(curr.down(), curr.leftDown(), curr.rightDown())
-            }
-        }
-        occupied.add(curr)
-        return curr
-    }
 
 
     companion object {
-        fun of(lines: List<String>): Cave2 {
+        fun of(lines: List<String>): Cave {
             val points = lines.flatMap{ l ->
                 l.split("->")
                     .map { p -> Point.of(p) }
@@ -95,17 +93,15 @@ data class Cave2(
 
             }.toMutableSet()
 
-            return Cave2(points)
+            return Cave(points)
 
         }
 
     }
 }
 
-fun main() {
-    val lines = readInputFile("src/main/kotlin/d14/input.dat")
-    val cave = Cave2.of(lines)
-
+fun solve1(lines: List<String>) {
+    val cave = Cave.of(lines)
     var sandPos = cave.dropSand()
     var sandUnit = 1
 
@@ -115,7 +111,24 @@ fun main() {
     }
 
     println(sandUnit - 1)
+}
 
+fun solve2(lines: List<String>) {
+    val cave = Cave.of(lines)
+    var sandPos = cave.dropSandP2()
+    var sandUnit = 1
 
+    while (sandPos != null) {
+        sandPos = cave.dropSandP2()
+        sandUnit += 1
+    }
+
+    println(sandUnit - 1)
+}
+
+fun main() {
+    val lines = readInputFile("src/main/kotlin/d14/input.dat")
+    solve1(lines)
+    solve2(lines)
 }
 
